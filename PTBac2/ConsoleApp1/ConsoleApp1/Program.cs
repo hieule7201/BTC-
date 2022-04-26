b9e0ddbbf7fb2d03c9145bc9ab8a15885d0ae3dd
hieule7201 <83806934+hieule7201@users.noreply.github.com>
2022-04-26 08:21:31 +0700

    Update Program.cs

--- PTBac2/ConsoleApp1/ConsoleApp1/Program.cs
+++ PTBac2/ConsoleApp1/ConsoleApp1/Program.cs
@@ -14,36 +14,36 @@ namespace ConsoleApp1
         }
     }
     class HeSo {
-        private int a;
-        private int b;
-        private int c;
+        private int a_121;
+        private int b_121;
+        private int c_121;
         public HeSo() { }
-        public HeSo(int a, int b, int c) {
-            this.a = a;
-            this.b = b;
-            this.c = c;
+        public HeSo(int a_121, int b_121, int c_121) {
+            this.a_121 = a_121;
+            this.b_121 = b_121;
+            this.c_121 = c_121;
         }
 
-        public int A { get => a; set => a = value; }
-        public int B { get => b; set => b = value; }
-        public int C { get => c; set => c = value; }
+        public int A { get => a_121; set => a_121 = value; }
+        public int B { get => b_121; set => b_121 = value; }
+        public int C { get => c_121; set => c_121 = value; }
 
 
-        public double Delta(int a, int b, int c)
+        public double Delta(int a_121, int b_121, int c_121)
         {
 
-            return b * b - 4 * a*c;
+            return b_121 * b_121 - 4 * a_121*c_121;
         }
-        public void Nghiem(int a, int b, int c) {
-            double del = Delta(a, b, c); 
-            if (del == 0)
-                Console.WriteLine("PT 1 nghiem x: {0}", -b / 2*a);
-            else if (del > 0)
+        public void Nghiem(int a_121, int b_121, int c_121) {
+            double del_121 = Delta(a_121, b_121, c_121); 
+            if (del_121 == 0)
+                Console.WriteLine("PT 1 nghiem x: {0}", -b_121 / 2*a_121);
+            else if (del_121 > 0)
             {
-                double x1 = (-b + Math.Sqrt(del)) / (2 * a);
-                double x2 = (-b - Math.Sqrt(del)) / (2 * a);
-                Console.WriteLine("x1 = {0}",x1);
-                Console.WriteLine("x2 = {0}",x2);
+                double x1_121 = (-b_121 + Math.Sqrt(del_121)) / (2 * a_121);
+                double x2_121 = (-b_121 - Math.Sqrt(del_121)) / (2 * a_121);
+                Console.WriteLine("x1 = {0}",x1_121);
+                Console.WriteLine("x2 = {0}",x2_121);
             }
             else
                 Console.WriteLine("PT vo nghiem");
